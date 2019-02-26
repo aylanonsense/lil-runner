5b7f936b455e663ac4738695ea74a0ab6bde2bd7
bridgs <bridgs.dev@gmail.com>
2019-02-26 01:50:05 -0500

    Spikes

--- main.lua
+++ main.lua
@@ -4,11 +4,12 @@ local GAME_HEIGHT = 200
 local RENDER_SCALE = 3
 
 -- Game constants
-local PLATFORM_WIDTH = 80
+local PLATFORM_WIDTH = 88
 local MOVE_SPEED = 80
 
 -- Game objects
 local duck
+local spikes
 local platforms
 local rightmostPlatform
 
@@ -35,13 +36,16 @@ function love.load()
     walkTimer = 0.00
   }
 
+  -- Create an empty array for spikes
+  spikes = {}
+
   -- Create the ground, which is made up of a series of platforms
   platforms = {}
   for i = 0, 1 + GAME_WIDTH / PLATFORM_WIDTH do
     table.insert(platforms, {
       x = i * PLATFORM_WIDTH,
       width = PLATFORM_WIDTH,
-      height = 55
+      height = 48
     })
   end
   rightmostPlatform = platforms[#platforms]
@@ -59,11 +63,24 @@ function love.update(dt)
     if platform.x + platform.width < 0 then
       platform.x = rightmostPlatform.x + rightmostPlatform.width
       local height = rightmostPlatform.height
-      if math.random() < 0.7 then
-        height = height + (math.random() < 0.5 and -10 or 10)
+      if math.random() < 0.8 then
+        height = math.min(math.max(16, height + (math.random() < 0.5 and -16 or 16)), 80)
       end
       platform.height = height
       rightmostPlatform = platform
+      -- Spawn some spikes
+      if math.random() < 0.6 then
+        local numSpikes = math.random(3, 6)
+        local arrangedVertically = math.random() < 0.5
+        local x = rightmostPlatform.x + rightmostPlatform.width / math.random(1, 2) - 5
+        local y = GAME_HEIGHT - platform.height - (math.random() < 0.5 and 2 or 18)
+        for i = 0, numSpikes - 1 do
+          table.insert(spikes, {
+            x = x - (arrangedVertically and 0 or 8 * i),
+            y = y - (arrangedVertically and 8 * i or 0)
+          })
+        end
+      end
     end
   end
 
@@ -74,13 +91,28 @@ function love.update(dt)
   duck.vy = duck.vy + 400 * dt
   duck.y = duck.y + math.min(duck.vy * dt, 4.5)
 
+  -- Move spikes to the left
+  for i = #spikes, 1, -1 do
+    local spike = spikes[i]
+    spike.x = spike.x - MOVE_SPEED * dt
+    if spike.x < -10 then
+      table.remove(spikes, i)
+    end
+    -- Check for collisions with the duck
+    local dx, dy = spike.x - duck.x, spike.y - duck.y
+    local dist = math.sqrt(dx * dx + dy * dy)
+    if dist < 7 then
+      bumpDuck()
+    end
+  end
+
   -- Check for collisions with the ground
   for _, platform in ipairs(platforms) do
     local platformY = GAME_HEIGHT - platform.height
     if platform.x <= duck.x and duck.x < platform.x + platform.width and duck.y > platformY then
       -- If the duck is too far into the ground, that means it failed to jump in time
       if duck.y > platformY + 5 then
-        duck.y = 0
+        bumpDuck()
       -- Otherwise, move it up on top of the platform
       else
         duck.y = platformY
@@ -103,12 +135,12 @@ function love.draw()
   love.graphics.scale(RENDER_SCALE, RENDER_SCALE)
 
   -- Clear the screen
-  love.graphics.setColor(0 / 255, 206 / 255, 244 / 255)
+  love.graphics.setColor(15 / 255, 217 / 255, 246 / 255)
   love.graphics.rectangle('fill', 0, 0, GAME_WIDTH, GAME_HEIGHT)
   love.graphics.setColor(1, 1, 1)
 
   -- Draw the platforms
-  love.graphics.setColor(22 / 255, 0 / 255, 45 / 255)
+  love.graphics.setColor(37 / 255, 2 / 255, 72 / 255)
   for _, platform in ipairs(platforms) do
     love.graphics.rectangle('fill', platform.x, GAME_HEIGHT - platform.height, platform.width, platform.height)
   end
@@ -132,6 +164,12 @@ function love.draw()
   end
   love.graphics.setColor(1, 1, 1)
   drawImage(duckImage, 16, 16, spriteNum, duck.x - 8, duck.y - 16)
+
+  -- Draw the spikes
+  love.graphics.setColor(137 / 255, 2 / 255, 72 / 255)
+  for _, spike in ipairs(spikes) do
+    love.graphics.circle('fill', spike.x, spike.y - 3.5, 3.5)
+  end
 end
 
 -- Draws a sprite from a sprite sheet image, spriteNum=1 is the upper-leftmost sprite
@@ -157,4 +195,9 @@ function love.keypressed(key)
       duck.numMidairJumps = 0
     end
   end
+end
+
+-- Bump the duck after it gets hit by an obstacle
+function bumpDuck()
+  duck.y = 0
 end
